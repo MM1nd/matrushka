35036639fb6d8ad4e673a94f5224485803043d0c
MM1nd <alexkornrumpf@web.de>
2022-06-25 17:56:36 +0200

    First Redux Question

---
+++ src/lib/component/QuestionRevealAnswer.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { Icon } from '@steeze-ui/svelte-icon';
+	import { ChevronRight, ChevronDoubleDown, QuestionMarkCircle } from '@steeze-ui/heroicons';
+
+	let revealed = false;
+
+	function toggleReveal() {
+		revealed = !revealed;
+	}
+</script>
+
+<div class="m-2">
+	<div class="flex bg-rose-50 p-1">
+		<div class="flex-none"><Icon src={QuestionMarkCircle} class="block h-4 w-4 m-1" /></div>
+		<div class="font-serif text-justify">
+			<slot name="question" />
+		</div>
+	</div>
+	<button
+		type="button"
+		class="w-full p-1 bg-rose-100 text-left"
+		class:hidden={revealed}
+		on:click={toggleReveal}
+	>
+		<Icon src={ChevronRight} class="inline-block h-4 w-4 ml-1 mb-1" />
+		Reveal Answer
+	</button>
+	<div class="flex bg-rose-100 p-1" class:hidden={!revealed}>
+		<div class="flex-none hover:cursor-pointer" on:click={toggleReveal}>
+			<Icon src={ChevronDoubleDown} class="block h-4 w-4 m-1" />
+		</div>
+		<div class="font-serif text-justify">
+			<slot name="answer" />
+		</div>
+	</div>
+</div>
--- src/routes/threec.svelte
+++ src/routes/threec.svelte
@@ -1,4 +1,5 @@
 <script>
+	import QuestionRevealAnswer from '$lib/component/QuestionRevealAnswer.svelte';
 	import RouteLink from '$lib/component/RouteLink.svelte';
 	import WipWarning from '$lib/component/WipWarning.svelte';
 	import { t } from '$lib/translations';
@@ -6,9 +7,31 @@
 
 <h2 class="text-3xl font-semibold m-2">{$t('common.headings.threec')}</h2>
 <WipWarning />
-<h3 class="text-2xl font-semibold m-2">Context</h3>
 <p class="m-2 font-serif text-justify">
 	This challenge assumes that you have already solved <RouteLink route_name="three" />
 	and <RouteLink route_name="threeb" />. It is much harder and much more open ended than either of
-	those challenges.
+	those challenges. We will be considering questions which, in my mind, arise naturally from
+	thinking about the two challenges in comparison. These are questions that I did not myself knew
+	the answer to when starting to write this page so they are a reflection of my thought process.
+	This doesn't mean that this is the one correct way to think about the two challenges or that there
+	is only one correct answer to these questions. I encourage you to think about questions that have
+	come to your own mind after solving the two challenges and use the ones below as a guideline only.
 </p>
+
+<QuestionRevealAnswer>
+	<p slot="question">
+		Compare the two challenges mentioned above. What similarities and differences come to mind? Do
+		all of the differences actually change the nature of the challenge?
+	</p>
+	<p slot="answer">
+		Hopefully you saw that the rules of the game are the same in both versions of the game. This
+		might be slightly obscured by the fact that one version asks you to go from alternating checkers
+		to sorted checkers and the other version asks you to go from sorted to alternating checkers.
+		However, we can make the argument that every legal move is reversible and there is nothing that
+		makes one direction inherently more interesting than the other. If we ignore the direction, the
+		challenges differ in the number of checkers involved and, perhaps not surprisingly, more
+		checkers require more moves to be reordered. The total number of space required does not differ
+		and is ten positions in both cases. This might spark your curiosity and will be the subject of
+		another question below.
+	</p>
+</QuestionRevealAnswer>
